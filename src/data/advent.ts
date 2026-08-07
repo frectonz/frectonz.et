a94388718a0965eb7a948b5729102a32c50039ac
frectonz <fraol0912@gmail.com>
2026-08-07 19:28:34 +0300

    feat: add advent of code section

---
+++ src/data/advent.ts
@@ -0,0 +1,22 @@
+export const advent = [
+  {
+    year: "2021",
+    days: 5,
+    code: "https://github.com/frectonz/advent_of_code_2021",
+  },
+  {
+    year: "2022",
+    days: 14,
+    code: "https://github.com/frectonz/advent_of_code_2022",
+  },
+  {
+    year: "2023",
+    days: 21,
+    code: "https://github.com/frectonz/advent_of_code_2023",
+  },
+  {
+    year: "2025",
+    days: 12,
+    code: "https://github.com/frectonz/advent_of_code_2025",
+  },
+];
