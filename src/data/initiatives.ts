1f82448d83f83e2cf92ae9786dafa27295d87cd4
frectonz <fraol0912@gmail.com>
2026-08-07 20:19:41 +0300

    feat: add a `Personal Initiatives` section

---
+++ src/data/initiatives.ts
@@ -0,0 +1,16 @@
+export const initiatives = [
+  {
+    name: "Overdue Progress",
+    code: "https://github.com/frectonz/overdueprogress.org",
+    url: "https://overdueprogress.org",
+    description:
+      "A publication dedicated to understanding how progress happens and how to make more of it, with a focus on Africa.",
+  },
+  {
+    name: "The /dev/tty Prize",
+    code: "https://github.com/frectonz/dev-tty",
+    url: "https://tty.frectonz.et/",
+    description:
+      "A prize competition for terminal software, open to anyone who wants to build a CLI or TUI app.",
+  },
+];
